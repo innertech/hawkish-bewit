66612693949d1beb4d7a424ea745d9f0b627bbb9
Raman Gupta <rocketraman@gmail.com>
2025-11-05 11:54:41 -0500

    1.4.1-SNAPSHOT

--- buildSrc/src/main/kotlin/LibraryConstants.kt
+++ buildSrc/src/main/kotlin/LibraryConstants.kt
@@ -1,6 +1,6 @@
 object LibraryConstants {
   const val group = "tech.inner"
   const val owner = "innertech"
-  const val versionName = "1.4.0"
+  const val versionName = "1.4.1-SNAPSHOT"
   const val versionCode = 1
 }
